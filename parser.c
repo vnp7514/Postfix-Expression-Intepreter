42c386954ff576767d04e80eb36eacb0fc0236e7
Van Nguyen Dinh Pham <vnp7514@cs.rit.edu>
2019-11-03 23:12:39 -0500

    parse() is correct when it is given the example from the project description

--- parser.c
+++ parser.c
@@ -7,16 +7,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "parser.h"
+#include <stdbool.h>
+#include <string.h>
 
 /// From parser.h
 ///
 void rep(char *exp){
 }
 
+/// Check whether the token is an operation
+/// @param token the token to be checked
+/// @return true if token is an operation, false otherwise
+///
+bool is_op(char *token){
+    return strcmp(token, ADD_OP_STR) == 0 ||
+        strcmp(token, SUB_OP_STR) == 0 ||
+        strcmp(token, MUL_OP_STR) == 0 ||
+        strcmp(token, DIV_OP_STR) == 0 ||
+        strcmp(token, MOD_OP_STR) == 0 ||
+        strcmp(token, Q_OP_STR) == 0 ||
+        strcmp(token, ASSIGN_OP_STR) == 0;
+}
+
+/// Convert the token into the enum op_type_e
+/// @param token the token
+/// @return a value within the op_type_t enum
+///
+op_type_t toOP(char* token){
+    if (strcmp(token, ADD_OP_STR) == 0){
+        return ADD_OP;
+    } else if (strcmp(token, SUB_OP_STR) == 0){
+        return SUB_OP;
+    } else if (strcmp(token, MUL_OP_STR) == 0){
+        return MUL_OP;
+    } else if (strcmp(token, DIV_OP_STR) == 0){
+        return DIV_OP;
+    } else if (strcmp(token, MOD_OP_STR) == 0){
+        return MOD_OP;
+    } else if (strcmp(token, ASSIGN_OP_STR) == 0){
+        return ASSIGN_OP;
+    } else if (strcmp(token, Q_OP_STR) == 0){
+        return Q_OP;
+    } else {
+        fprintf(stderr, "Invalid operation string!\n");
+	exit(EXIT_FAILURE); 
+    }
+}
+
 /// From parser.h
 ///
 tree_node_t *parse(stack_t *stack){
-    return NULL;
+    tree_node_t* node = NULL;
+    char *token = top(stack);
+    pop(stack);
+    if (is_op(token)){
+        op_type_t op = toOP(token);
+	tree_node_t* left = parse(stack);
+	tree_node_t* right = parse(stack);
+	node = make_interior(op, token, left, right);
+    } else {
+        char *endptr;
+        strtol(token, &endptr, 10);
+        if (endptr == token){// Should be a symbol
+           node = make_leaf(SYMBOL, token);
+        } else if (*endptr != 0){//there are characters after number
+           fprintf(stderr, "BAD NUMBER in parse() in parser.c\n");
+           exit(EXIT_FAILURE);
+        } else {
+	   node = make_leaf(INTEGER, token); 
+	}
+    }
+    return node;
 }
 
 /// From parser.h
@@ -38,11 +99,11 @@ void print_infix(tree_node_t* node){
         if (node->type == LEAF){
             printf("%s", node->token);
         } else {
-            tree_node_t* next = node->node;
+            interior_node_t* next = (interior_node_t*) node->node;
             printf("(");
-	    print_infix(next->left);
-	    printf("%s", node->token);
-	    print_infix(next->right);
+            print_infix(next->left);
+            printf("%s", node->token);
+            print_infix(next->right);
             printf(")");
         }
     } else {
@@ -65,14 +126,14 @@ void free_interior(interior_node_t* node){
 /// From parser.h
 ///
 void cleanup_tree(tree_node_t* node){
-    if (tree != NULL){
-        free(tree->token);
-        if (tree->type == INTERIOR){
-            free_interior(tree->node);
+    if (node != NULL){
+        free(node->token);
+        if (node->type == INTERIOR){
+            free_interior(node->node);
         } else {
-            free(tree->node); 
+            free(node->node); 
         }
-        free(tree);
+        free(node);
     }
 }
 
